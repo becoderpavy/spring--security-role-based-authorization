a144492ffae247824dbc66ef32a2dc7607806dcc
Pabitra Das <codewithpabitra@gmail.com>
2024-09-25 01:17:58 +0530

    inital commit

---
+++ src/main/java/com/becoder/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.becoder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.becoder.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	public User findByUsername(String username);
+	
+}
